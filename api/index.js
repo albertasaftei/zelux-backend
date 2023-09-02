4dd72cb8b16b57d0fe3e8df9afee497503aeecd7
alburt <albert.asaftei00@gmail.com>
2023-09-02 11:59:49 +0200

    test - using router as endpoints

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
-const app = require("express")();
+import { Router } from "express";
+var router = Router();
 
-app.get("/api", (req, res) => {
+router.get("/api", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.end(`Hello!`);
 });
 
-app.get("/api/item/:slug", (req, res) => {
+router.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
 
-module.exports = app;
+export default router;
