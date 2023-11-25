a0be2bf4d0cb23b67d76f38f44eec77ad609a7b9
alburt <albert.asaftei00@gmail.com>
2023-11-25 13:16:46 +0100

    🧪

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-const router = express.Router();
+import { Router } from "express";
+var router = Router();
+
+router.get("/api", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello!`);
+});
 
 router.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
-  console.log("/////////", slug);
-  res.json(`Item: ${slug}`);
+  res.end(`Item: ${slug}`);
 });
+
+export default router;
