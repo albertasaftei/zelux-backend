55bdc687f161870ff81c45275d4ac212a9a1807a
alburt <albert.asaftei00@gmail.com>
2024-03-16 12:40:04 +0100

    🧪

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 import serviceAccount from "./credentials.json" assert { type: "json" };
 
 // Initialize Firebase
@@ -6,5 +7,6 @@ export const firebaseAdminApp =
   getApps().length === 0
     ? initializeApp({ credential: cert(serviceAccount) })
     : getApp();
-
 console.log("Firebase initialized");
+
+export const db = getFirestore(firebaseAdminApp);
