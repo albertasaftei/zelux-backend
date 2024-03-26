d562d2f5370921985c0ac2e99d1a3b082c6ba2c6
alburt <albert.asaftei00@gmail.com>
2024-03-26 19:22:44 +0100

    🔒️ moved firebase credentials to env

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,22 @@
 import { initializeApp, cert } from "firebase-admin/app";
-import serviceAccount from "./credentials.json" assert { type: "json" };
+
+const credentials = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com",
+};
 
 // Initialize Firebase
 export const firebaseAdminApp = initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert(credentials),
 });
 
 console.log("--------------------------------------------");
